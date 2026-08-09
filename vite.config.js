e03277cde4da2caa60281548040ff340ebb5418f
Zreps <ritzxyz08@gmail.com>
2026-08-09 22:09:16 +0700

    chore: perbiakan vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite' 
-import prerender from '@mr-hope/vite-plugin-prerender' 
 
 export default defineConfig({
   plugins: [
     react(),    
     tailwind(),
-    prerender({
-      routes: ['/'], s
-    }),
     {
       name: 'html-seo-plugin',
       transformIndexHtml(html) {
